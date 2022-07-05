e1ebfca254fd9625846fdc2fef15e1a283ccadb6
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2022-07-05 17:04:29 -0400

    Updated for code server 4.5.0

--- maincontainer/build.sh
+++ maincontainer/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+VERSION=4.5.0
 podman build . -t quay.io/gpte-devops-automation/codeserver:latest
+podman tag quay.io/gpte-devops-automation/codeserver:latest quay.io/gpte-devops-automation/codeserver:v${VERSION}
 podman push quay.io/gpte-devops-automation/codeserver:latest
+podman push quay.io/gpte-devops-automation/codeserver:v${VERSION}
