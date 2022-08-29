768ec0aa07bd274d3586fbb61f3a6866c9422cf5
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2022-08-29 13:52:32 -0400

    Fix build tags

--- maincontainer/build.sh
+++ maincontainer/build.sh
@@ -4,4 +4,4 @@ JAVA_VERSION=11
 podman build . -t quay.io/gpte-devops-automation/codeserver:${VERSION}-java${JAVA_VERSION}
 podman tag quay.io/gpte-devops-automation/codeserver:${VERSION}-java${JAVA_VERSION} quay.io/gpte-devops-automation/codeserver:latest
 podman push quay.io/gpte-devops-automation/codeserver:latest
-podman push quay.io/gpte-devops-automation/codeserver:v${VERSION}-java${JAVA_VERSION}
+podman push quay.io/gpte-devops-automation/codeserver:${VERSION}-java${JAVA_VERSION}
