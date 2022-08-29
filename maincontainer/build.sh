53d71c42fdd290acaf1c2945ce0be07f6f44a1a6
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2022-08-29 13:40:42 -0400

    Update to include OpenJDK devel, support different java versions during build

--- maincontainer/build.sh
+++ maincontainer/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-VERSION=4.6.0
-podman build . -t quay.io/gpte-devops-automation/codeserver:latest
-podman tag quay.io/gpte-devops-automation/codeserver:latest quay.io/gpte-devops-automation/codeserver:v${VERSION}
+VERSION=v4.6.0
+JAVA_VERSION=11
+podman build . -t quay.io/gpte-devops-automation/codeserver:${VERSION}-java${JAVA_VERSION}
+podman tag quay.io/gpte-devops-automation/codeserver:latest quay.io/gpte-devops-automation/codeserver:v${VERSION}-java-${JAVA_VERSION}
 podman push quay.io/gpte-devops-automation/codeserver:latest
-podman push quay.io/gpte-devops-automation/codeserver:v${VERSION}
+podman push quay.io/gpte-devops-automation/codeserver:v${VERSION}-${JAVA_VERSION}
