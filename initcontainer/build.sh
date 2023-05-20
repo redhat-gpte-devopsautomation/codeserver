d249c60aa9a4af2ced60a40c87bcad931528c981
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2023-05-20 10:58:45 +0200

    Updated for Code Server 4.13.0

--- initcontainer/build.sh
+++ initcontainer/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=4.12.0
+VERSION=4.13.0
 podman build . -t quay.io/gpte-devops-automation/codeserver-init:latest
 podman tag quay.io/gpte-devops-automation/codeserver-init:latest quay.io/gpte-devops-automation/codeserver-init:v${VERSION}
 podman push quay.io/gpte-devops-automation/codeserver-init:latest
