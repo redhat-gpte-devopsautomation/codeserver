f01ac1cdabd85f0175a847b677c0a8c225dfc988
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2023-07-29 14:50:43 +0200

    Updated for Code Server v4.16.0

--- maincontainer/build.sh
+++ maincontainer/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=v4.15.0
+VERSION=v4.16.0
 JAVA_VERSION=11
 podman build . -t quay.io/gpte-devops-automation/codeserver:${VERSION}-java${JAVA_VERSION}
 podman tag quay.io/gpte-devops-automation/codeserver:${VERSION}-java${JAVA_VERSION} quay.io/gpte-devops-automation/codeserver:latest
