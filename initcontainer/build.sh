65f53a691b50782e5687cec9904faf9784b47e78
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2023-10-01 12:33:26 +0200

    Updated for 1.17.1

--- initcontainer/build.sh
+++ initcontainer/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-VERSION=4.17.0
+VERSION=4.17.1
 podman build . -t quay.io/gpte-devops-automation/codeserver-init:latest
 podman tag quay.io/gpte-devops-automation/codeserver-init:latest quay.io/gpte-devops-automation/codeserver-init:v${VERSION}
 podman push quay.io/gpte-devops-automation/codeserver-init:latest
