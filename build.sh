c898253a504d330b3df095f42396c4b6f675cc0f
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2023-11-20 11:48:53 -0500

    Fix versions

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-CODE_SERVER_VERSION=v4.19.0
+CODE_SERVER_VERSION="4.19.0"
+IMAGE_VERSION="v${CODE_SERVER_VERSION}"
 JAVA_VERSION=11
 MAVEN_VERSION="3.8.8"
 OCP_VERSION="4.14"
@@ -11,9 +12,9 @@ podman build . \
   --build-arg BUILD_DATE=${BUILD_DATE} \
   --tag quay.io/gpte-devops-automation/codeserver-init:latest
 
-podman tag quay.io/gpte-devops-automation/codeserver-init:latest quay.io/gpte-devops-automation/codeserver-init:v${CODE_SERVER_VERSION}
+podman tag quay.io/gpte-devops-automation/codeserver-init:latest quay.io/gpte-devops-automation/codeserver-init:${IMAGE_VERSION}
 podman push quay.io/gpte-devops-automation/codeserver-init:latest
-podman push quay.io/gpte-devops-automation/codeserver-init:v${CODE_SERVER_VERSION}
+podman push quay.io/gpte-devops-automation/codeserver-init:${IMAGE_VERSION}
 
 cd ../maincontainer
 podman build . \
@@ -22,9 +23,9 @@ podman build . \
   --build-arg MAVEN_VERSION=${MAVEN_VERSION} \
   --build-arg OCP_VERSION=${OCP_VERSION} \
   --build-arg BUILD_DATE=${BUILD_DATE} \
-  --tag quay.io/gpte-devops-automation/codeserver:${CODE_SERVER_VERSION}-java${JAVA_VERSION}
-podman tag quay.io/gpte-devops-automation/codeserver:${CODE_SERVER_VERSION}-java${JAVA_VERSION} quay.io/gpte-devops-automation/codeserver:latest
+  --tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
+podman tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION} quay.io/gpte-devops-automation/codeserver:latest
 podman push quay.io/gpte-devops-automation/codeserver:latest
-podman push quay.io/gpte-devops-automation/codeserver:${CODE_SERVER_VERSION}-java${JAVA_VERSION}
+podman push quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
 
 cd ..
