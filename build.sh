be63f22851622b3b7ba6b40952f671cb595ba979
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2024-06-12 13:44:01 +0200

    Add Java 21 Images

--- build.sh
+++ build.sh
@@ -23,8 +23,8 @@ podman build . \
   --build-arg OCP_VERSION=${OCP_VERSION} \
   --build-arg JAVA_VERSION="11" \
   --build-arg BUILD_DATE=${BUILD_DATE} \
-  --tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java11
-podman push quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java11
+  --tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
+podman push quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
 
 # Build for Java 17
 podman build . \
@@ -33,9 +33,19 @@ podman build . \
   --build-arg OCP_VERSION=${OCP_VERSION} \
   --build-arg JAVA_VERSION="17" \
   --build-arg BUILD_DATE=${BUILD_DATE} \
-  --tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java17
-podman tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java17 quay.io/gpte-devops-automation/codeserver:latest
+  --tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
+podman push quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
+
+# Build for Java 21
+podman build . \
+  --build-arg CODE_SERVER_VERSION=${CODE_SERVER_VERSION} \
+  --build-arg MAVEN_VERSION=${MAVEN_VERSION} \
+  --build-arg OCP_VERSION=${OCP_VERSION} \
+  --build-arg JAVA_VERSION="21" \
+  --build-arg BUILD_DATE=${BUILD_DATE} \
+  --tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
+podman push quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
+podman tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION} quay.io/gpte-devops-automation/codeserver:latest
 podman push quay.io/gpte-devops-automation/codeserver:latest
-podman push quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java17
 
 cd ..
