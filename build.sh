683949a56a3f6bb4f27748f498f0e1529e1a3c34
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2024-06-13 14:04:30 +0200

    Fix java version for tags

--- build.sh
+++ build.sh
@@ -17,31 +17,34 @@ podman push quay.io/gpte-devops-automation/codeserver-init:${IMAGE_VERSION}
 
 cd ../maincontainer
 # Build for Java 11
+JAVA_VERSION=11
 podman build . \
   --build-arg CODE_SERVER_VERSION=${CODE_SERVER_VERSION} \
   --build-arg MAVEN_VERSION=${MAVEN_VERSION} \
   --build-arg OCP_VERSION=${OCP_VERSION} \
-  --build-arg JAVA_VERSION="11" \
+  --build-arg JAVA_VERSION="${JAVA_VERSION}" \
   --build-arg BUILD_DATE=${BUILD_DATE} \
   --tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
 podman push quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
 
 # Build for Java 17
+JAVA_VERSION=17
 podman build . \
   --build-arg CODE_SERVER_VERSION=${CODE_SERVER_VERSION} \
   --build-arg MAVEN_VERSION=${MAVEN_VERSION} \
   --build-arg OCP_VERSION=${OCP_VERSION} \
-  --build-arg JAVA_VERSION="17" \
+  --build-arg JAVA_VERSION="${JAVA_VERSION}" \
   --build-arg BUILD_DATE=${BUILD_DATE} \
   --tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
 podman push quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
 
 # Build for Java 21
+JAVA_VERSION=21
 podman build . \
   --build-arg CODE_SERVER_VERSION=${CODE_SERVER_VERSION} \
   --build-arg MAVEN_VERSION=${MAVEN_VERSION} \
   --build-arg OCP_VERSION=${OCP_VERSION} \
-  --build-arg JAVA_VERSION="21" \
+  --build-arg JAVA_VERSION="${JAVA_VERSION}" \
   --build-arg BUILD_DATE=${BUILD_DATE} \
   --tag quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
 podman push quay.io/gpte-devops-automation/codeserver:${IMAGE_VERSION}-java${JAVA_VERSION}
